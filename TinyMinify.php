e9cacb83a79f8a3ad34e19471561f27a359d9204
Anton S <tony.smirnov@gmail.com>
2022-07-30 09:37:51 +0300

    Update TinyMinify.php
    Add tag "<cms:minify_page />"; Fix "disable_comments" parameter.

--- TinyMinify.php
+++ TinyMinify.php
@@ -1,7 +1,7 @@
 <?php
 
-if (!defined('K_COUCH_DIR')) die(); // cannot be loaded directly
-require 'TinyHtmlMinifier.php';
+if( ! defined('K_COUCH_DIR') ) die(); // cannot be loaded directly
+if( ! class_exists('TinyHtmlMinifier') ) require_once( K_ADDONS_DIR.'tiny-html-minifier/TinyHtmlMinifier.php' );
 
 class TinyMinify
 {
@@ -12,22 +12,54 @@ class TinyMinify
 
         $options = $FUNCS->get_named_vars(
             array(
-                'collapse_whitespace' => "false",
-                'disable_comments' => "false"
+                'disable_comments' => 0,
+                'collapse_whitespace' => 0,
             ), $params);
 
-        foreach ($options as &$option) {
-            $option= (int) filter_var($option, FILTER_VALIDATE_BOOLEAN);
-        }
+        $options = array_map( function(&$option){ return (bool) $option; }, $options);
+        $options['disable_comments'] ^= 1;
 
         foreach ($node->children as $child) {
             $html .= $child->get_HTML();
         }
 
-
         $minifier = new TinyHtmlMinifier($options);
         return $minifier->minify($html);
     }
+
+    public static function minify_page( $params, $node )
+    {
+        global $FUNCS, $PAGE;
+        if( count($node->children) ) {die("ERROR: Tag \"".$node->name."\" is a self closing tag");}
+
+        $options = $FUNCS->get_named_vars(
+                       array(
+                              'disable_comments' => 0,
+                              'collapse_whitespace' => 0,
+                             ),
+                       $params
+                    );
+
+        $options = array_map( function(&$option){ return (bool) $option; }, $options);
+        $options['disable_comments'] ^= 1;
+
+        $PAGE->minify = 1;
+        $PAGE->minify_options = $options;
+    }
+
+    public static function alter_minify(&$html, $PAGE, $k_cache_file, $redirect_url, $content_type_header)
+    {
+        if( ! is_null($redirect_url) ){ return; }
+
+        // comment following to always compress all pages without tag
+        if( ! isset($PAGE->minify) && $PAGE->minify !== 1 ){ return; }
+
+        $minifier = new TinyHtmlMinifier($PAGE->minify_options);
+        $html = $minifier->minify($html);
+    }
+
 }
 
-$FUNCS->register_tag('minify', array('TinyMinify', 'minify'));
+$FUNCS->register_tag( 'minify', array('TinyMinify', 'minify') );
+$FUNCS->register_tag( 'minify_page', array('TinyMinify', 'minify_page') );
+$FUNCS->add_event_listener( 'alter_final_page_output', array('TinyMinify', 'alter_minify') );
